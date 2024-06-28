f71dece2a4a3d3e694760de2a2c4f327505bf2d9
Katumbela <ja3328173@gmail.com>
2024-06-29 00:01:58 +0100

    start the api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-import express from 'express'; 
+import express from 'express';
 import userRoutes from './routes/userRoutes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/users', userRoutes);
+app.use('/', () => 'Bem vindo a Multischool API');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
