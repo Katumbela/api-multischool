7677666632f6e05b85568ea7d6864aa653b788d5
Katumbela <ja3328173@gmail.com>
2024-06-29 15:06:12 +0100

    config swagger and sequelize

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,31 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: API para listar de usuários
+ */
+
 import { Router } from 'express';
 import { getUsers } from '../controllers/userController';
 
 const userRoutes = Router();
+
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Retorna todos os usuários
+ *     tags: [Users]
+ *     responses:
+ *       '200':
+ *         description: Lista de usuários
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
 
 userRoutes.route('/users')
   .get(getUsers)
