84b369851fe241f52fb98f2da73a0860a0b347dd
Katumbela <ja3328173@gmail.com>
2024-06-29 15:43:19 +0100

    seed datas

--- config/database.js
+++ config/database.js
@@ -6,20 +6,23 @@ module.exports = {
     "password": "zIApsI1ZiE",
     "database": "sql8716902",
     "host": "sql8.freesqldatabase.com",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "dialectModule": require('mysql') // Use 'mysql' instead of 'mysql'
   },
   "test": {
     "username": "sql8716902",
     "password": "zIApsI1ZiE",
     "database": "sql8716902",
     "host": "sql8.freesqldatabase.com",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "dialectModule": require('mysql') // Use 'mysql' instead of 'mysql'
   },
   "production": {
     "username": "sql8716902",
     "password": "zIApsI1ZiE",
     "database": "sql8716902",
     "host": "sql8.freesqldatabase.com",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "dialectModule": require('mysql') // Use 'mysql' instead of 'mysql'
   }
-}
+};
